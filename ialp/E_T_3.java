fe0d30dbcc881c34297c0ba1d91cdbe7177d0e2a
Meyer Pidiache <meyer.pidiache@gmail.com>
2022-10-17 17:36:40 -0500

    E_T_A

--- ialp/E_T_3.java
+++ ialp/E_T_3.java
@@ -1,23 +1,41 @@
+/* Primera parte:
+
+- Crear una función con tres parámetros que sean números que se suman entre sí.
+
+- Llamar a la función en el main y darle valores.
+
+- Segunda parte:
+
+- Crear una clase coche.
+
+- Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+
+- Una función que incremente el número de puertas que tiene el coche.
+
+- Crear un objeto miCoche en el main y añadirle una puerta.
+
+- Mostrar el número de puertas que tiene el objeto. */
+
 public class E_T_3 {
     public static void main(String[] args) {
         // Primera parte
-        System.out.println(suma(1,2,3));
-        
+        System.out.println(suma(1, 2, 3));
+
         // Segunda parte
         Coche miCoche = new Coche();
         miCoche.mPuertas();
         System.out.println(miCoche.nPuertas);
     }
-    
-    public static int suma(int a, int b, int c){
+
+    public static int suma(int a, int b, int c) {
         return a + b + c;
     }
 }
 
 class Coche {
-        public int nPuertas = 2;
-        
-        public void mPuertas (){
-            this.nPuertas++;
-        }
+    public int nPuertas = 2;
+
+    public void mPuertas() {
+        this.nPuertas++;
     }
+}
--- ialp/E_T_8.java
+++ ialp/E_T_8.java
@@ -1,10 +1,22 @@
-public class Main {
+/* Para practicar la encapsulación:
+
+- Crear clase Persona.
+
+- Crear variables las privadas edad, nombre y telefono de la clase Persona.
+
+- Crear gets y sets de cada propiedad.
+
+- Crear un objeto persona en el main.
+
+- Utiliza los gets y sets para darle valores a las propiedades edad, nombre y telefono, por último muéstralas por consola. */
+
+public class E_T_8 {
     public static void main(String[] args) {
         Persona estudiante = new Persona();
         estudiante.setEdad(17);
         estudiante.setNombre("Meyer");
         estudiante.setTelefono(314592333);
-        
+
         System.out.println("Edad del estudiante: " + estudiante.getEdad());
         System.out.println("Nombre del estudiante: " + estudiante.getNombre());
         System.out.println("Telefono del estudiante: " + estudiante.getTelefono());
@@ -15,28 +27,28 @@ class Persona {
     private int edad;
     private String nombre;
     private int telefono;
-    
-    public int getEdad(){
+
+    public int getEdad() {
         return this.edad;
     }
-    
-    public void setEdad(int edad){
+
+    public void setEdad(int edad) {
         this.edad = edad;
     }
-    
-    public String getNombre(){
+
+    public String getNombre() {
         return this.nombre;
     }
-    
-    public void setNombre(String nombre){
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
-    public int getTelefono(){
+
+    public int getTelefono() {
         return this.telefono;
     }
-    
-    public void setTelefono(int telefono){
+
+    public void setTelefono(int telefono) {
         this.telefono = telefono;
     }
 }
