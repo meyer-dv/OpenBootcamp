72dd39dc78fb854ef88be8f0350be27980d9ea36
Meyer Pidiache <meyer.pidiache@gmail.com>
2022-12-23 13:56:00 -0500

    e_3.py

--- cdp/e_3.py
+++ cdp/e_3.py
@@ -1,5 +1,5 @@
 peso = float(input("Igresa tu peso en Kg: "))
 estatura = float(input("Ingresa tu estatura en metros: "))
-indiceMasaCorporal = round(peso * estatura ** 2, 2)
+indiceMasaCorporal = round(peso / estatura ** 2, 2)
 
 print("Tu índice de masa corporal es", indiceMasaCorporal)
