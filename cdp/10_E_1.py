7714975b5e02c4e9b4b817cc39885881f0ebfeb7
Meyer Pidiache <meyer.pidiache@gmail.com>
2023-01-19 15:42:16 -0500

    10_E_1

--- cdp/10_E_1.py
+++ cdp/10_E_1.py
@@ -4,46 +4,55 @@ En este ejercicio tenéis que crear una lista de RadioButton que muestre la opci
 Al principio no tiene que haber una opción seleccionada.
 """
 
-import tkinter as tk
+from tkinter import Tk, StringVar, TOP, mainloop
+from tkinter.ttk import Radiobutton, Button, Label
+
+# Ventana
+window = Tk()
+window.title("MEVyer")
+window.resizable(width=False, height=False)
 
 
-def main():
-    elements = {
-        "1. Primer opción": "1",
-        "2. Segunda opción": "2",
-        "3. Tercer opción": "3",
-        "4. Cuarta opción": "4",
-    }
+# Tkinter string variable
+# able to store any string value
+v = StringVar(window)
 
-    window = tk.Tk()
-    window.title("OpenBootcamp")
-    window.columnconfigure(0, minsize=200)
+# Diccionario con diferentes opciones
+elements = {
+    1: "Primer opción",
+    2: "Segunda opción",
+    3: "Tercer opción",
+    4: "Cuarta opción",
+}
 
-    # Radio buttons
-    selected = tk.StringVar()
-    counter = 0
-    for (i, j) in elements.items():
-
-        radioButton = tk.Radiobutton(
-            window, text=i, value=j, variable=selected, font=("Times New Roman", 12)
-        )
-
-        radioButton.grid(row=counter, columnspan=2, sticky="ew", padx=5)
-
-        counter += 1
-
-    # Button
-    def reset(event):
-        print("Reset")
-        selected.set("")
-
-    button = tk.Button(window, text="Reset")
-    button.bind("<Button-1>", reset)
-    button.grid(row=4)
-
-    window.columnconfigure(0, weight=1)
-    window.mainloop()
+# Label
+def setLabel():
+    label.config(text=f"-> {elements[int(v.get())]} <-")
 
 
-if __name__ == "__main__":
-    main()
+# Radio Buttons
+for (value, text) in elements.items():
+    Radiobutton(window, text=text, variable=v, command=setLabel, value=value, width=15,).pack(
+        side=TOP,
+        padx=10,
+        pady=5,
+    )
+
+
+# Button on click
+def reset(event):
+    v.set("")
+    label.config(text="-> <-")
+
+
+# Label
+label = Label(text="-> <-")
+label.pack(pady=5,)
+
+# Button
+button = Button(window, text="Reset")
+button.bind("<Button-1>", reset)
+button.pack(pady=5,)
+
+
+mainloop()
