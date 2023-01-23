eaaf86a7ae2e9ca24cb70c395f552a3dc54c258d
Meyer Pidiache <meyer.pidiache@gmail.com>
2023-01-23 09:11:18 -0500

    12_E_1

--- cdp/12_E_1/filmmakers/settings.py
+++ cdp/12_E_1/filmmakers/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config as dec
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l=^=bgz0%2yhejaue3&4_y8q0d^8!-o(@8s$@-h$0p%#$0(bti"
+SECRET_KEY = dec("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
