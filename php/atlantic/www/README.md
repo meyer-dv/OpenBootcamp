f4bbdb75db02d72d121ee9d8e7625b26a96f3ec6
Meyer Pidiache <meyer.pidiache@gmail.com>
2023-04-15 17:20:17 -0500

    PHP 8

--- php/atlantic/www/README.md
+++ php/atlantic/www/README.md
@@ -4,6 +4,10 @@
 docker compose up --build -d
 ```
 
+### Configuración de PHP
+
+Se ingresa al contenedor `php_7.4_app` y se inicia el proyecto
+
 ```shell
 docker exec -it php_7.4_app bash
 ```
@@ -12,18 +16,32 @@ docker exec -it php_7.4_app bash
 composer init
 ```
 
-composer.json
+Se agrega `phpunit` al composer.json
+
 ```shell
+    [...]
     "require-dev": {
         "phpunit/phpunit": "^9.5"
     }
 ```
 
 ```shell
-docker install
+composer install
 ```
 
 ## Levantar contenedores
 ```shell
 docker compose up -d
+```
+
+## Tumbar contenedores
+
+```shell
+docker compose down
+```
+
+## Regenerar contenedores
+
+```shell
+docker compose build --no-cache
 ```
--- php/atlantic/www/src/index.php
+++ php/atlantic/www/src/index.php
@@ -1,5 +1,54 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-phpinfo();
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-?>
+<body>
+    <h1>PHP Basic</h1>
+    <?php
+    $name = "Meyer";
+    $num = 2 /* A comment */+ 3;
+    echo "Hello $name<br>";
+
+    print "Hey, this is print. att: $name" . $num . "<br><br>";
+
+    echo "<b>array(1, 2, 3, array('a', 5, TRUE))</b><br>";
+    $arr = array(1, 2, 3, array("a", 5, TRUE));
+    print_r($arr);
+    echo "<br>";
+    var_dump($arr);
+    ?>
+
+    <h2>Funciones</h2>
+    <?php
+    function get_name()
+    {
+        global $name;
+        $GLOBALS['num'] = $GLOBALS['num'] + 2;
+        echo $name . $GLOBALS['num'];
+    }
+
+    get_name();
+
+    echo "<h3>Static</h3>";
+
+    function get_some()
+    {
+        static $st = 1;
+        echo "<li>" . $st . "<br>";
+        $st++;
+    }
+
+    get_some();
+    get_some();
+
+    ?>
+
+</body>
+
+</html>
